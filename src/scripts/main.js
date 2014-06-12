16ce55a9356d3de919dc2e5ba544417cdbbb5115
Zach Smith <x.coder.zach@gmail.com>
2014-06-12 14:37:10 -0400

    a few changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,15 +19,13 @@ function page(axis, evt) {
 }
 
 phys.css(function(position) {
-  var scale = initialScale + (position.y / height) * (1 - initialScale)
+  lastY = position.y
+  lastX = position.x
+
+  var scale = lastScale = scaleFromY(position.y)
   layout(position.x, scale)
   return {}
 })
-
-var body = {
-  position: Vector(50, 50),
-  velocity: Vector(3, 50)
-}
 
 var fingerOffsetX, fingerOffsetY
   , chosenIndex
@@ -37,22 +35,26 @@ var startEvent = touchEnabled ? 'touchstart' : 'mousedown'
 var endEvent = touchEnabled ? 'touchend' : 'mouseup'
 var moveEvent = touchEnabled ? 'touchmove' : 'mousemove'
 
+var scaleOffset
+
 window.addEventListener(startEvent, function(evt) {
+  phys.cancel()
   mousedown = true
   veloX = new Velocity()
   veloY = new Velocity()
 
-  fingerOffsetX = page('x', evt)
-  fingerOffsetY = page('y', evt)
+  fingerOffsetX = page('x', evt) - lastX
+  fingerOffsetY = page('y', evt) - lastY
+  scaleOffset = lastScale
 
   var target = evt.target
   for(var i = 0 ; (target = target.previousSibling) != null ; target.nodeType === 1 && i++) {}
   chosenIndex = i
 })
 
-var lastX
-  , lastY
-  , lastScale
+var lastX = 0
+  , lastY = 0
+  , lastScale = .25
 
 window.addEventListener(endEvent, function(evt) {
 
@@ -60,7 +62,7 @@ window.addEventListener(endEvent, function(evt) {
 
   var vel = Vector(veloX.getVelocity() || 0, veloY.getVelocity() || 0)
 
-  if(vel.y <= 0)
+  if(vel.y < 0)
     phys.spring(vel, { x: lastX, y: lastY }, { x: 0, y: 0 }, { k: 20, b: 300 })
   else
     phys.spring(vel, { x: lastX, y: lastY }, { x: -chosenIndex * width, y: height }, { k: 20, b: 300 })
@@ -69,23 +71,35 @@ window.addEventListener(endEvent, function(evt) {
 function loop() {
   requestAnimationFrame(function() {
     if(mousedown) {
-      layout(lastX, lastScale)
+      var scale = lastScale = scaleFromY(lastY)
+      layout(lastX, scale)
     }
     loop()
   })
 }
 loop()
 
+function interpolator(start, end) {
+  return function(value) {
+    return (value - start) / (end - start)
+  }
+}
+
+function scaleFromY(y) {
+  var f = interpolator(0, height)
+  return 0.25 + f(y) * 0.75
+}
+
+window.interpolator = interpolator
 
 window.addEventListener(moveEvent, function(evt) {
   evt.preventDefault()
   if(mousedown) {
     var y = lastY = page('y', evt) - fingerOffsetY
-    var scale = lastScale = initialScale + y / height
-    var x = lastX = (page('x', evt) - (fingerOffsetX * (scale/initialScale)))
+      , scale = scaleFromY(y)
+      , x = lastX = page('x', evt) - (fingerOffsetX * scale/scaleOffset)
 
     veloX.updatePosition(x)
     veloY.updatePosition(y)
-
   }
 })
